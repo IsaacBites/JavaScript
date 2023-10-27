4920b3ed37362dc51b9bd876d18bd29f95fe4466
IsaacBites <isaacbites80@gmail.com>
2023-10-27 10:40:37 -0200

    Create aula5
    Criação do arquivo ex008.js
    Lógica simples

---
+++ aula5/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60.4
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60.5){
+    console.log(`Velocidade acima do permitido`)
+}else if(vel == 60.5){
+    console.log(`Limite de velocidade`)
+}else{
+    console.log(`Velocidade permitida`)
+}
