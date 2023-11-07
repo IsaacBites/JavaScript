d191252e4a4dd2f555370247b761bf1aca539814
IsaacBites <isaacbites80@gmail.com>
2023-11-07 16:27:57 -0200

    Update Projeto_idade
    Finalização do projeto
    Adição das fotos no código
    Adição dos links para contato

--- Exercicios/Projeto_idade/script.js
+++ Exercicios/Projeto_idade/script.js
@@ -8,13 +8,41 @@ function verificar(){
     }else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/garotinho.png')
+            }else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            }else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulto.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
         }else if(fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/garotinha.png')
+            }else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-f.png')
+            }else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulta.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
