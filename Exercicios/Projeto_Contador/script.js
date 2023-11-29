18053dc6fa28d49e4f3f407b43284052dcd02258
IsaacBites <isaacbites80@gmail.com>
2023-11-29 09:05:33 -0200

    Update Projeto_Contador
    Alterações no layout do projeto
    Alterações no estilo do projeto
    Melhoria da lógica de programação

--- Exercicios/Projeto_Contador/script.js
+++ Exercicios/Projeto_Contador/script.js
@@ -3,7 +3,7 @@ function contar(){
     let ini = window.document.getElementById('idinicio')
     let fim = window.document.getElementById('idfim')
     let pas = window.document.getElementById('idpasso')
-    let msg = window.document.getElementById('msg')
+    let msg = window.document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
         window.alert('[ERRO] Faltam dados!')
@@ -13,9 +13,19 @@ function contar(){
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-        for(var c = i; c <= f; c += p){
-            msg.innerHTML +=  `${c} `
+        if(i < f){
+            for(var c = i; c <= f; c += p){
+                msg.innerHTML +=  `${c} `
+            }            
+        }else{
+            for(var c = i; c >= f; c -= p){
+                msg.innerHTML +=  `${c} `
+            } 
         }
+
+
+
+
     }
 
 }
