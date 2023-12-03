ace978e8189d3b84457792242a6253bc48bb2779
IsaacBites <isaacbites80@gmail.com>
2023-12-03 10:35:53 -0200

    Criação da Aula 8
    Introdução a vetores no JavaScript

---
+++ aula8/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 3, 8, 0]
+num[3] = 6
+num.push(7) /*Adiciona o número na última posição do array*/
+
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+num.sort() /*Coloca as posições do vetor em ordemos*/
+console.log(`O vetor em ordem ${num}`)
