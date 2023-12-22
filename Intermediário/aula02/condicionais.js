58054fed76f5fded3e7d1f78c54ecfb68385e76a
IsaacBites <isaacbites80@gmail.com>
2023-12-22 18:29:17 -0200

    Update aula02
    Criação do código do arquivo condicionais.js

---
+++ Intermediário/aula02/condicionais.js
@@ -0,0 +1,12 @@
+const isAzul = true
+const isCamiseta = false
+
+// '===' compara os tipos de váriáveis
+
+//Boa pratica, variáveis booleanas coloque "is" na frente na hora de nomear 
+
+if (isAzul == true){
+    console.log('verdadeiro')
+}else{
+    console.log('Falso')
+}
