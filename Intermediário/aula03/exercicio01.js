cd2a3f4be583e90b2c799c5ed2eefdfe02a65bc1
IsaacBites <isaacbites80@gmail.com>
2023-12-29 19:46:04 -0200

    Update Intermediário
    Criação da pasta aula03
    Criação do arquivo exercicio01.js
    Adição do enunciado

---
+++ Intermediário/aula03/exercicio01.js
@@ -0,0 +1,12 @@
+/*
+
+1) Faça um algoritimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade calcule e imprima a sua média e aa sua classificação conforme a tabela abaixo:
+
+Média = (nota 1 + nota 2 + nota 3) / 3
+
+Classificação:
+ - Média menos que 5, reprovação;
+ - Média entre 5 e 7, recuperação;
+ - Média acima de 7, passou de semestre 
+
+*/
