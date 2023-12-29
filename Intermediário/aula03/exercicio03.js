81f421abdaea806493d63dc708cb51ee48a995af
IsaacBites <isaacbites80@gmail.com>
2023-12-29 20:35:05 -0200

    Update aula03
    adição da resolução do arquivo exercicio03.js

--- Intermediário/aula03/exercicio03.js
+++ Intermediário/aula03/exercicio03.js
@@ -12,7 +12,37 @@ Código Confição de pagamento:
 */
 
 
+console.log('Informe o código( 1 - Débito; 2 - Dinheiro ou PIX; 3 - Parcelado):')
+const cod = 3
+const preco = 10.0
 
+if(cod == 1){
+    precoFinal = preco - (preco * 0.1)
+    console.log('Desconto de 10%')
+    console.log(precoFinal)
+}else if(cod == 2){
+    precoFinal = preco - (preco * 0.15)
+    console.log('Desconto de 15%')
+    console.log(precoFinal)
+}else if(cod == 3){
+    console.log('Deseja Parcelar em quantas vezes?')
+    const par = 3
+
+    if(par == 1){
+        precoFinal = preco - (preco * 0.1)
+        console.log('Desconto de 10%')
+        console.log(precoFinal)
+    }else if(par == 2){
+        console.log('Preço normal sem Juros')
+        console.log(preco)  
+    }else{
+        precoFinal = preco + (preco * 0.1)
+    console.log('Desconto de 10%')
+    console.log(precoFinal)
+    }
+}else{
+    console.log('Código inválido')
+}
 
 
 
