46b9b8c689e9dab641898e8b65b2ca6ee130055a
IsaacBites <isaacbites80@gmail.com>
2023-12-29 20:20:19 -0200

    Update aula03
    Criação do arquivo exercicio03.js

---
+++ Intermediário/aula03/exercicio03.js
@@ -0,0 +1,30 @@
+/*
+
+3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+Utilize s códigos da tabela a seguir para ler qual a condição de pagamento escolhida para efetuar o cálculo adequado.
+
+Código Confição de pagamento:
+ - À vista Débito, recebe 10% de desconto;
+ - À vista no Dinheiro ou PIX, recebe 15% de desconto;
+ - Em duas vezes, preço normal de etiqueta sem juros;
+ - Acima de duas vezes, preço normal de etiqueta mais juros de 10%; 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
