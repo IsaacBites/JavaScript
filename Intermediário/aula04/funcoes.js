66138da056cee847ec9afe6eeeedc65da705508c
IsaacBites <isaacbites80@gmail.com>
2023-12-31 15:25:02 -0200

    Update aula04
    Adição do código de exemplo no arquivo funcoes.js

---
+++ Intermediário/aula04/funcoes.js
@@ -0,0 +1,14 @@
+function sayMyName(name){
+    console.log('Your name is ' + name)
+}
+
+sayMyName('Ricardo')   //Chama a função
+
+
+
+function Quadrado(valor){
+    return valor * valor
+}
+
+const area = Quadrado(10)
+console.log(area)
