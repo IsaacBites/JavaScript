9f77e48f81589231e07f2adcd50cbccb6018a8a7
IsaacBites <isaacbites80@gmail.com>
2024-01-06 18:03:34 -0200

    Update aula05
    Criação do código do arquivo exercicio03.js

---
+++ Intermediário/aula05/exercicio03.js
@@ -0,0 +1,54 @@
+/*
+
+3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+Utilize s códigos da tabela a seguir para ler qual a condição de pagamento escolhida para efetuar o cálculo adequado.
+
+Código Confição de pagamento:
+ - À vista Débito, recebe 10% de desconto;
+ - À vista no Dinheiro ou PIX, recebe 15% de desconto;
+ - Em duas vezes, preço normal de etiqueta sem juros;
+ - Acima de duas vezes, preço normal de etiqueta mais juros de 10%; 
+
+*/
+
+function exibirInfo(){
+    console.log('Informe o código( 1 - Débito; 2 - Dinheiro ou PIX; 3 - Parcelado):')
+}
+
+function calcularOpcoes(cod, preco){
+    if(cod == 1){
+        precoFinal = preco - (preco * 0.1)
+        console.log('Desconto de 10%')
+        console.log(precoFinal)
+    }else if(cod == 2){
+        precoFinal = preco - (preco * 0.15)
+        console.log('Desconto de 15%')
+        console.log(precoFinal)
+    }else if(cod == 3){
+        console.log('Deseja Parcelar em quantas vezes?')
+        const par = 2
+    
+        if(par == 1){
+            precoFinal = preco - (preco * 0.1)
+            console.log('Desconto de 10%')
+            console.log(precoFinal)
+        }else if(par == 2){
+            console.log('Preço normal sem Juros')
+            console.log(preco)  
+        }else{
+            precoFinal = preco + (preco * 0.1)
+        console.log('Desconto de 10%')
+        console.log(precoFinal)
+        }
+    }else{
+        console.log('Código inválido')
+    }
+}
+
+(function (){
+    exibirInfo()
+    calcularOpcoes(3, 10.0)
+})()
+
+
+
