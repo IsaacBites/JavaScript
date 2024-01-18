abb9b82970681728806303d0d8f6ef57ecdc6cb8
IsaacBites <isaacbites80@gmail.com>
2024-01-18 11:53:09 -0200

    Update aula07
    Adição do conteúdo no arquivo arrays02.js

---
+++ Intermediário/aula07/arrays02.js
@@ -0,0 +1,21 @@
+const aluno = ['João', 'Vitor', 'Marina']
+
+aluno.push('Renan') //Adiciona um novo elemento no final do array
+
+aluno[4] = 'Ricardo' //Adiciona um novo elemento diretamente na posição do array
+
+console.log(aluno)
+
+aluno.pop() //Remove o último elemento do array
+
+console.log(aluno)
+
+aluno.shift()  //Remove o primeiro elemento do array
+
+console.log(aluno)
+
+
+
+
+
+
