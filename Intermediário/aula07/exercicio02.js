d5ec8dc9574a721e202a496376a3abd1c72822c0
IsaacBites <isaacbites80@gmail.com>
2024-01-18 14:00:50 -0200

    Update aula07
    Adição da solução do exercício02.js

--- Intermediário/aula07/exercicio02.js
+++ Intermediário/aula07/exercicio02.js
@@ -1,1 +1,9 @@
 // 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada númeoro Par encontrado
+
+const v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < v.length; i++) {
+    if (v[i] % 2 === 0) {
+        console.log(v[i])
+    }
+}
